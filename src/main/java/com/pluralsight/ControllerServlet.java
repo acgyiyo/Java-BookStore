c018d3ce031f652abdc7132208f4a478b5c3f608
acgyiyo <acgyiyo1@gmail.com>
2019-06-04 15:43:57 -0500

    add functionality to show editForm to update books

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -63,6 +63,10 @@ public class ControllerServlet extends HttpServlet {
                     break;
                 case "/delete":
                     deleteBook(request, response);
+                    break;
+                case "/edit":
+                    showEditForm(request, response);
+                    break;
                 default:
                     listBooks(request, response);
                     break;
@@ -71,6 +75,14 @@ public class ControllerServlet extends HttpServlet {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Book book = bookDAO.getBook(id);
+        RequestDispatcher rd = request.getRequestDispatcher("/BookForm.jsp");
+        request.setAttribute("book", book);
+        rd.forward(request, response);
     }
 
     private void deleteBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
